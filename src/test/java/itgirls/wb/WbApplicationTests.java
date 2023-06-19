568bd6ae34cb8e1422ade85c14260d4c18199b06
nemanov.ivan <nemanov.ivan@otr.ru>
2023-06-19 19:03:50 +0300

    Initial commit

---
+++ src/test/java/itgirls/wb/WbApplicationTests.java
@@ -0,0 +1,13 @@
+package itgirls.wb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
