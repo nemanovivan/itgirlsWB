35fb132022a6a7b8e4643c4c85716c20bc9c8b5d
AnnaTretyakova <anntretwork@gmail.com>
2023-07-10 02:20:43 +0200

    перезаписан метод toString для WeatherDto, использован новый способ формирования uri в GeoLocatorClient, что решает проблему с использованием русскоязычных адресов, добавлена документация, добавлен TestController для дебага

--- src/main/java/itgirls/wb/http/client/GeoLocatorClient.java
+++ src/main/java/itgirls/wb/http/client/GeoLocatorClient.java
@@ -1,20 +1,42 @@
 package itgirls.wb.http.client;
 
 import itgirls.wb.http.dto.GeoLocatorDto;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
+
+import org.apache.hc.core5.net.URIBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeoLocatorClient {
+
+    /**
+     * клиент использует restTemplate, определенный в ClientConfiguration
+     */
     private final RestTemplate restTemplate;
+    /**
+     * клиент использует url, определенный в ClientConfiguration
+     */
     private final String url;
 
+    /**
+     * язык ответа определяется двузначным кодом языка_двузначным кодом страны
+     */
     private final String language = "ru_RU";
 
-    static final int AMOUNT_OF_RESULTS = 1;
+    /**
+     * константа, хранящая максимальное количество возвращаемых объектов
+     */
+    static final String AMOUNT_OF_RESULTS = "1";
 
+    /**
+     * API-ключ, полученный на сайте яндекса, хранится в application.properties
+     */
     @Value("${apikeyGeoCoder}")
     private String apiKey;
 
@@ -23,16 +45,30 @@ public class GeoLocatorClient {
         this.url = url;
     }
 
+    /**
+     * в методе getCoordinatesFrom() формируется uri,
+     * по сформированному uri отправляетя get запрос
+     *
+     * @param address - адрес, указанный пользователем
+     * @return GeoLocatorDto для заданного адресса
+     */
     public GeoLocatorDto getCoordinatesFrom(String address) {
-        URI uri = UriComponentsBuilder.fromUriString(url)
-                .queryParam("apikey", apiKey)
-                .queryParam("geocode", address)
-                .queryParam("format", "json")
-                .queryParam("results", AMOUNT_OF_RESULTS)
-                .queryParam("lang", language)
-                .build()
-                .toUri();
-        GeoLocatorDto coordinates = restTemplate.getForObject(uri, GeoLocatorDto.class);
+        //form parameters
+        List<NameValuePair> arrayListOfParameters = new ArrayList<>();
+        arrayListOfParameters.add(new BasicNameValuePair("apikey", apiKey));
+        arrayListOfParameters.add(new BasicNameValuePair("geocode", address));
+        arrayListOfParameters.add(new BasicNameValuePair("format", "json"));
+        arrayListOfParameters.add(new BasicNameValuePair("results", AMOUNT_OF_RESULTS));
+        arrayListOfParameters.add(new BasicNameValuePair("lang", language));
+
+        URI uri = null;
+        try {
+            uri = new URIBuilder(new URI(url))
+                    .addParameters(arrayListOfParameters)
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
         return restTemplate.getForObject(uri, GeoLocatorDto.class);
     }
--- src/main/java/itgirls/wb/http/dto/WeatherDto.java
+++ src/main/java/itgirls/wb/http/dto/WeatherDto.java
@@ -7,18 +7,39 @@ import java.util.List;
 
 public record WeatherDto(String date, Fact fact, Forecast forecast) {
 
+
+    @Override
+    public String toString() {
+        return "Здравствуйте! Погода " + this.date + " в вашем регионе следующая: \n" +
+                "температура: " + this.fact.temp + " градусов" + "\n" +
+                "температура ощущается как: " + this.fact.tempFeelsLike + " градусов" + "\n" +
+                "погодные условия: " + this.fact.condition + "\n" +
+                "скорость ветра: " + this.fact.windSpeed + " м/c" + "\n" +
+                "направление ветра: " + this.fact.windDir + "\n" +
+                "порывы ветра: " + "до " + this.fact.windGust + " м/c" + "\n" +
+                "влажность: " + this.fact.humidity + "%" + "\n" +
+                "\n" +
+                "средняя температура в первой половине дня: " + this.forecast.partsList.get(0).temp + "градусов" + "\n" +
+                "средняя температура во второй половине дня: " + this.forecast.partsList.get(1).temp + "градусов" + "\n" +
+                "\n" +
+                "встречайте рассвет в " + this.forecast.sunrise + "\n" +
+                "встречайте закат в " + this.forecast.sunset + "\n";
+    }
+
+
     @JsonCreator
     public WeatherDto(
             @JsonProperty("now_dt") String date,
             @JsonProperty("fact") Fact fact,
             @JsonProperty("forecast") Forecast forecast
-    ){
+    ) {
         this.date = date;
         this.fact = fact;
         this.forecast = forecast;
     }
 
-    public record Fact(int temp, int tempFeelsLike, String condition, int windSpeed, String windDir, float windGust, int humidity) {
+    public record Fact(int temp, int tempFeelsLike, String condition, int windSpeed, String windDir, float windGust,
+                       int humidity) {
         @JsonCreator
         public Fact(
                 @JsonProperty("temp") int temp,
